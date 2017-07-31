b215b0370aabf6e7c00df1d2aedc6b1f057f9719
birkin <birkin.diana@gmail.com>
2017-07-31 13:43:42 -0400

    modernizes url syntax.

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+from illiad_app import views
 
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
 
-    url( r'^admin/', include(admin.site.urls) ),  # eg host/project_x/admin/
+    url( r'^admin/', include(admin.site.urls) ),
 
-    url( r'^', include('illiad_app.urls_app') ),  # eg host/project_x/anything/
+    url( r'^info/$', views.info, name='info_url' ),
 
-)
+    url( r'^v2/make_request/$', views.make_request_v2, name='request_v2' ),
+
+    url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
+
+]
