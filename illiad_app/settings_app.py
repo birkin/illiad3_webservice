91533a098c80f10d28b3e031d443f921ebaa93f1
birkin <birkin.diana@gmail.com>
2018-11-27 15:56:05 -0500

    adds docs setting.

--- illiad_app/settings_app.py
+++ illiad_app/settings_app.py
@@ -3,6 +3,8 @@
 import json, os
 
 
+README_URL = os.environ['ILLIAD_WS__DOCS']
+
 TEST_AUTH_KEY = os.environ['ILLIAD_WS__API_AUTH_KEY']
 
 TEST_EXISTING_GOOD_USER = os.environ['ILLIAD_WS__TEST_EXISTING_GOOD_USER']
