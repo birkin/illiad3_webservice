1d2ab2fbc083b5e2da8a91da69578fe4ffae3736
birkin <birkin.diana@gmail.com>
2019-03-11 13:05:38 -0400

    stubs initial make-request handler module.

---
+++ illiad_app/lib/make_request_v3.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+Submits ILLiad request via hitting the ILLiad API instead of simulating browser GETs and POSTs.
+Implemented in preparation for ILLiad cloud switch-over.
+"""
+
+import datetime, logging, pprint, random, time
+from illiad_app import settings_app
+
+
+log = logging.getLogger(__name__)
+
+
+class MakeRequestManager( object ):
+
+    def __init__( self, request_id ):
+        # self.request_id = random.randint( 1111, 9999 )  # to follow logic if simultaneous hits
+        self.request_id = request_id
+
+    def check_validity( self, request ):
+        """ Checks post, auth_key, ip, & params.
+            Called by views.make_request_v3() """
+        log.debug( '%s - starting check_validity()' % self.request_id )
+        return_val = False
+        if request.method == 'POST':
+            if self.check_params( request ) is True:
+                if request.POST['auth_key'] == self.API_KEY:
+                    return_val = True
+                else:
+                    log.debug( '%s - ip, `%s`' % (self.request_id, request.META.get('REMOTE_ADDR', 'unavailable')) )
+        log.debug( '%s - return_val, `%s`' % (self.request_id, return_val) )
+        return return_val
+
+    ## end class MakeRequestManager()
