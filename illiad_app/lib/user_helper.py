667232cc584c5e97eba976485417464067702026
birkin <birkin.diana@gmail.com>
2019-03-18 13:18:39 -0400

    adds data-check.

--- illiad_app/lib/user_helper.py
+++ illiad_app/lib/user_helper.py
@@ -8,8 +8,18 @@ from illiad_app.lib.illiad3.account import Status as LibStatusModule
 log = logging.getLogger(__name__)
 
 
-class UserHandler( object ):
+class UserHelper( object ):
 
     def __init__( self ):
         self.request_id = random.randint( 1111, 9999 )  # to follow logic if simultaneous hits
         self.status_module = LibStatusModule( settings_app.ILLIAD_REMOTE_AUTH_URL, settings_app.ILLIAD_REMOTE_AUTH_KEY )
+
+    def data_check( self, request ):
+        """ Checks data.
+            Called by views.check_user() """
+        log.debug( '%s - request.GET, `%s`' % (self.request_id, request.GET) )
+        return_val = 'invalid'
+        if 'user' in request.GET.keys():
+            return_val = 'valid'
+        log.debug( '%s - return_val, `%s`' % (self.request_id, return_val) )
+        return return_val
