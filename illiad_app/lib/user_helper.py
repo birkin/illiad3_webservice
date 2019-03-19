f1819635cc797e754316973867027223d31210f8
birkin <birkin.diana@gmail.com>
2019-03-19 09:49:27 -0400

    removing commented-out code.

--- illiad_app/lib/user_helper.py
+++ illiad_app/lib/user_helper.py
@@ -13,11 +13,6 @@ class UserHelper( object ):
 
     def __init__( self ):
         self.request_id = random.randint( 1111, 9999 )  # to follow logic if simultaneous hits
-        # self.output_dct = None
-        # self.output_dct = {
-        #     'user': None,
-        #     'response': {'authenticated': None, 'registered': None, 'blocked': None, 'disavowed': None}
-        #     }
 
     def data_check( self, request ):
         """ Checks data.
@@ -28,20 +23,6 @@ class UserHelper( object ):
             return_val = 'valid'
         log.debug( '%s - return_val, `%s`' % (self.request_id, return_val) )
         return return_val
-
-    # def manage_check( self, request, start_time ):
-    #     """ Runs login and response evaluation.
-    #         Called by views.check_user() """
-    #     user = request.GET['user']
-    #     log.debug( '%s - user, `%s`' % (self.request_id, user) )
-    #     self.output_dct['user'] = user
-    #     illiad_session = IlliadSession( settings_app.ILLIAD_REMOTE_AUTH_URL, settings_app.ILLIAD_REMOTE_AUTH_KEY, user )
-    #     login_response_dct = illiad_session.login()
-    #     for key in login_response_dct:
-    #         if key in self.output_dct['response'].keys():  # ignores session_id
-    #             self.output_dct['response'][key] = login_response_dct[key]
-    #     log.debug( '%s - output_dct, ```%s```' % (self.request_id, pprint.pformat(self.output_dct)) )
-    #     return self.output_dct
 
     def manage_check( self, request, start_time ):
         """ Runs login and response evaluation.
@@ -76,7 +57,7 @@ class UserHelper( object ):
                     request.META['QUERY_STRING'] ),  # REQUEST_URI contains querystring but doesn't exist via run-server
                 'timestamp': str( start_time ) },
             'response': self.prep_response_segment( start_time, data_dct ) }
-        log.debug( 'initialized output_dct, ```%s```' % pprint.pformat(output_dct) )
+        log.debug( '%s - output_dct, ```%s```' % (self.request_id, pprint.pformat(output_dct)) )
         return output_dct
 
     def prep_response_segment( self, start_time, data_dct ):
