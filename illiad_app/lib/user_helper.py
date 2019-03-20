bf599050e24d65d59bbfef75c8aa504cb68aaae7
birkin <birkin.diana@gmail.com>
2019-03-20 11:29:56 -0400

    calls basic-auth code.

--- illiad_app/lib/user_helper.py
+++ illiad_app/lib/user_helper.py
@@ -14,13 +14,25 @@ class UserHelper( object ):
     def __init__( self ):
         self.request_id = random.randint( 1111, 9999 )  # to follow logic if simultaneous hits
 
+    # def data_check( self, request ):
+    #     """ Checks data.
+    #         Called by views.check_user() """
+    #     log.debug( '%s - request.GET, `%s`' % (self.request_id, request.GET) )
+    #     return_val = 'invalid'
+    #     if 'user' in request.GET.keys():
+    #         return_val = 'valid'
+    #     log.debug( '%s - return_val, `%s`' % (self.request_id, return_val) )
+    #     return return_val
+
     def data_check( self, request ):
         """ Checks data.
             Called by views.check_user() """
         log.debug( '%s - request.GET, `%s`' % (self.request_id, request.GET) )
         return_val = 'invalid'
         if 'user' in request.GET.keys():
-            return_val = 'valid'
+            log.debug( 'user param exists' )
+            if basic_auth_helper.check_basic_auth( request ) is True:
+                return_val = 'valid'
         log.debug( '%s - return_val, `%s`' % (self.request_id, return_val) )
         return return_val
 
