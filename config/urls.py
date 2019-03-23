263dfe83a367cb55a4b8cdc13683b516602fdb8b
birkin <birkin.diana@gmail.com>
2019-03-23 13:36:16 -0400

    adds comments.

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,11 @@ urlpatterns = [
 
     url( r'^v2/make_request/$', views.make_request_v2, name='request_v2' ),
 
-    url( r'^check_status_via_shib/$', views.check_status_via_shib, name='check_status_via_shib' ),
+    url( r'^check_status_via_shib/$', views.check_status_via_shib, name='check_status_via_shib' ),  # status meaning 'Undergrad', 'Staff', etc.
 
     url( r'^update_status/$', views.update_status, name='update_status' ),
 
-    url( r'^check_user/$', views.check_user, name='check_user' ),
+    url( r'^check_user/$', views.check_user, name='check_user' ),  # checks status meaning 'registered', 'blocked', etc.
 
     url( r'^error_check/$', views.error_check, name='error_check' ),  # only generates error if DEBUG == True
 
