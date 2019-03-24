daf3ddb462f7b45e5de561b6a978fe5d9fedd5ff
birkin <birkin.diana@gmail.com>
2019-03-24 12:04:49 -0400

    adds create-user url.

--- config/urls.py
+++ config/urls.py
@@ -12,16 +12,17 @@ urlpatterns = [
 
     # url( r'^admin/', include(admin.site.urls) ),
 
-    url( r'^info/$', views.info, name='info_url' ),
+
+    url( r'^check_user/$', views.check_user, name='check_user' ),  # checks status meaning 'registered', 'blocked', etc.
 
     url( r'^v2/make_request/$', views.make_request_v2, name='request_v2' ),
 
     url( r'^check_status_via_shib/$', views.check_status_via_shib, name='check_status_via_shib' ),  # status meaning 'Undergrad', 'Staff', etc.
-
     url( r'^update_status/$', views.update_status, name='update_status' ),
 
-    url( r'^check_user/$', views.check_user, name='check_user' ),  # checks status meaning 'registered', 'blocked', etc.
+    url( r'^create_user/$', views.create_user, name='create_user' ),
 
+    url( r'^info/$', views.info, name='info_url' ),
     url( r'^error_check/$', views.error_check, name='error_check' ),  # only generates error if DEBUG == True
 
     url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
