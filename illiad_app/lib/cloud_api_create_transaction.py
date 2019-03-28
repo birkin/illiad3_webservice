e6d793a48059a5995abddd41fc1235bbbe80edf0
birkin <birkin.diana@gmail.com>
2019-03-28 11:31:36 -0400

    implements transaction-create code.

--- illiad_app/lib/cloud_api_create_transaction.py
+++ illiad_app/lib/cloud_api_create_transaction.py
@@ -1,53 +1,41 @@
-# POST https://your.illiad.edu/ILLiadWebPlatform/Users
-# Accept-Type: application/json
+# POST https://your.illiad.edu/ILLiadWebPlatform/transaction/
+# Content-Type: application/json
 # {
-#   "Username" : "bdoe",
-#   "ExternalUserId": "Z20181116",
-#   "FirstName":"Bailey",
-#   "LastName":"Doe",
-#   "EmailAddress" : "bailey@test.com" ,
-#   "DeliveryMethod" : "Hold for Pickup",
-#   "LoanDeliveryMethod" : "Mail to Address",
-#   "NotificationMethod" : "Electronic",
-#   "Phone" : "757-123-4568",
-#   "Status" : "Graduate",
-#   "PlainTextPassword": "MySpecialPassword1234!",
-#   "AuthType" : "ILLiad",
-#   "Department" : "Music",
-#   "Web" : true,
-#   "Address" : "123 Oak St",
-#   "Address2" : "Apt 4E",
-#   "City" : "Virgia Beach",
-#   "State" : "VA",
-#   "Zip" : "23462"
+#  "Username" : "jdoe",
+#  "RequestType" : "Article",
+#  "ProcessType" : "Borrowing",
+#  "PhotoJournalTitle" : "Journal of Interlibrary Loan,Document Delivery & Electronic Reserve",
+#  "PhotoArticleTitle" : "Interlibrary Loan in the United States: An Analysis of Academic Libraries in a Digital Age",
+#  "PhotoArticleAuthor" : "Williams, Joseph; Woolwine, David",
+#  "PhotoJournalVolume" : "21",
+#  "PhotoJournalIssue" : "4",
+#  "PhotoJournalYear" : "2011",
+#  "PhotoJournalInclusivePages" : "165-183",
+#  "ISSN": "1072-303X",
+#  "TransactionStatus": "Awaiting Request Processing",
+#  "CopyrightAlreadyPaid": "Yes",
 # }
 
 
 import os, requests
 
 params = {
-  "Username" : "bdoe",
-  "ExternalUserId": "Z20181116",
-  "FirstName":"Bailey",
-  "LastName":"Doe",
-  "EmailAddress" : "birkin_diana@brown.edu" ,
-  "DeliveryMethod" : "Hold for Pickup",
-  "LoanDeliveryMethod" : "Mail to Address",
-  "NotificationMethod" : "Electronic",
-  "Phone" : "757-123-4568",
-  "Status" : "Graduate",
-  "PlainTextPassword": "MySpecialPassword1234!",
-  "AuthType" : "ILLiad",
-  "Department" : "Music",
-  "Web" : True,
-  "Address" : "123 Oak St",
-  "Address2" : "Apt 4E",
-  "City" : "Virgia Beach",
-  "State" : "VA",
-  "Zip" : "23462"
+ "Username" : "bdoe",
+ "RequestType" : "Article",
+ "ProcessType" : "Borrowing",
+ "PhotoJournalTitle" : "Journal of Interlibrary Loan,Document Delivery & Electronic Reserve",
+ "PhotoArticleTitle" : "Interlibrary Loan in the United States: An Analysis of Academic Libraries in a Digital Age",
+ "PhotoArticleAuthor" : "Williams, Joseph; Woolwine, David",
+ "PhotoJournalVolume" : "21",
+ "PhotoJournalIssue" : "4",
+ "PhotoJournalYear" : "2011",
+ "PhotoJournalInclusivePages" : "165-183",
+ "ISSN": "1072-303X",
+ "TransactionStatus": "Awaiting Request Processing",
+ "CopyrightAlreadyPaid": False,
 }
 
-url = '%s%s' % ( os.environ['ILLIAD_WS__OFFICIAL_ILLIAD_API_URL'], 'Users' )  # root url contains ending-slash
+url = '%s%s' % ( os.environ['ILLIAD_WS__OFFICIAL_ILLIAD_API_URL'], 'Transaction' )  # root url contains ending-slash
 print( 'url, ```%s```' % url )
 
 headers = {
