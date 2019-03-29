55bd43faf4c243d503d3c137f850970f7fe23a10
birkin <birkin.diana@gmail.com>
2019-03-29 09:53:32 -0400

    experiments with test-server api.

--- illiad_app/lib/cloud_api_create_user.py
+++ illiad_app/lib/cloud_api_create_user.py
@@ -1,50 +1,26 @@
-# POST https://your.illiad.edu/ILLiadWebPlatform/Users
-# Accept-Type: application/json
-# {
-#   "Username" : "bdoe",
-#   "ExternalUserId": "Z20181116",
-#   "FirstName":"Bailey",
-#   "LastName":"Doe",
-#   "EmailAddress" : "bailey@test.com" ,
-#   "DeliveryMethod" : "Hold for Pickup",
-#   "LoanDeliveryMethod" : "Mail to Address",
-#   "NotificationMethod" : "Electronic",
-#   "Phone" : "757-123-4568",
-#   "Status" : "Graduate",
-#   "PlainTextPassword": "MySpecialPassword1234!",
-#   "AuthType" : "ILLiad",
-#   "Department" : "Music",
-#   "Web" : true,
-#   "Address" : "123 Oak St",
-#   "Address2" : "Apt 4E",
-#   "City" : "Virgia Beach",
-#   "State" : "VA",
-#   "Zip" : "23462"
-# }
-
 
 import os, requests
 
 params = {
   "Username" : "bdoe",
-  "ExternalUserId": "Z20181116",
+  "ExternalUserId": "123456789",
   "FirstName":"Bailey",
   "LastName":"Doe",
-  "EmailAddress" : "birkin_diana@brown.edu" ,
+  "EmailAddress" : "test@test.test" ,
   "DeliveryMethod" : "Hold for Pickup",
   "LoanDeliveryMethod" : "Mail to Address",
   "NotificationMethod" : "Electronic",
-  "Phone" : "757-123-4568",
+  "Phone" : "123-456-7890",
   "Status" : "Graduate",
-  "PlainTextPassword": "MySpecialPassword1234!",
+  "PlainTextPassword": "zzzzzzzzzzzz",
   "AuthType" : "ILLiad",
   "Department" : "Music",
   "Web" : True,
-  "Address" : "123 Oak St",
-  "Address2" : "Apt 4E",
-  "City" : "Virgia Beach",
-  "State" : "VA",
-  "Zip" : "23462"
+  "Address" : "the address",
+  "Address2" : "extra address info",
+  "City" : "the city",
+  "State" : "RI",
+  "Zip" : "12345"
 }
 
 url = '%s%s' % ( os.environ['ILLIAD_WS__OFFICIAL_ILLIAD_API_URL'], 'Users' )  # root url contains ending-slash
@@ -59,5 +35,3 @@ r = requests.post( url, data=params, headers=headers, timeout=60, verify=True )
 print( r.status_code )
 
 print( r.content )
-
-
