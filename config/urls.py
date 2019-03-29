ad2b6844340841cce753b8caaf42041d7dc53631
birkin <birkin.diana@gmail.com>
2019-03-29 09:46:09 -0400

    enables url for cloud-api book request.

--- config/urls.py
+++ config/urls.py
@@ -12,16 +12,18 @@ urlpatterns = [
 
     # url( r'^admin/', include(admin.site.urls) ),
 
+    url( r'^check_user/$', views.check_user, name='check_user' ),  # (remote-auth method) checks status meaning 'registered', 'blocked', etc.
+
+    url( r'^v2/make_request/$', views.make_request_v2, name='request_v2' ),  # (remote-auth method) simulated web access to submit request
+
+    url( r'^check_status_via_shib/$', views.check_status_via_shib, name='check_status_via_shib' ),  # (remote-auth method) status meaning 'Undergrad', 'Staff', etc.
+    url( r'^update_status/$', views.update_status, name='update_status' ),  # (remote-auth method)
+
+    url( r'^create_user/$', views.create_user, name='create_user' ),
+
+    url( r'^cloud_book_request/$', views.cloud_book_request, name='cloud_book_request' ),  # (official illiad-api) submits request
+
     url( r'^info/$', views.info, name='info_url' ),
-
-    url( r'^v2/make_request/$', views.make_request_v2, name='request_v2' ),
-
-    url( r'^v3/make_book_request/$', views.make_book_request_v3, name='book_request_v3' ),
-
-    url( r'^check_status_via_shib/$', views.check_status_via_shib, name='check_status_via_shib' ),
-
-    url( r'^update_status/$', views.update_status, name='update_status' ),
-
     url( r'^error_check/$', views.error_check, name='error_check' ),  # only generates error if DEBUG == True
 
     url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
