50946b68b1f3e6ae91c27ef50f64d290e13ddab4
birkin <birkin.diana@gmail.com>
2019-03-29 09:55:02 -0400

    merges extra blocked check.

--- illiad_app/lib/illiad3/account.py
+++ illiad_app/lib/illiad3/account.py
@@ -46,13 +46,27 @@ class IlliadSession( object ):
         log.info( "ILLiad session `%s` established for `%s`; perceived registered-status: `%s`" % (self.session_id, self.username, self.registered) )
         return out
 
+    # def problem_check( self, out, resp_text ):
+    #     """ Manager for blocked-check and disavowed-check.
+    #         Called by login() """
+    #     log.debug( 'resp.text, ```%s```' % resp_text )
+    #     err = False
+    #     if self._check_blocked( resp_text ) == True:
+    #         out['blocked'] = True; err = True
+    #     elif self._check_disavowed( resp_text ) == True:
+    #         out['disavowed'] = True; err = True
+    #     log.debug( 'out, ```%s```; err, `%s`' % (pprint.pformat(out), err) )
+    #     return( out, err )
+
     def problem_check( self, out, resp_text ):
         """ Manager for blocked-check and disavowed-check.
             Called by login() """
         log.debug( 'resp.text, ```%s```' % resp_text )
         err = False
         if self._check_blocked( resp_text ) == True:
-            out['blocked'] = True; err = True
+            out['blocked'] = True; out['authenticated'] = True; out['registered'] = True; err = True
+        elif self.check_blocked_2() == True:
+            out['blocked'] = True; out['authenticated'] = True; out['registered'] = True; err = True
         elif self._check_disavowed( resp_text ) == True:
             out['disavowed'] = True; err = True
         log.debug( 'out, ```%s```; err, `%s`' % (pprint.pformat(out), err) )
@@ -61,7 +75,7 @@ class IlliadSession( object ):
     def _check_blocked( self, resp_text ):
         """ Checks if login attempt indicates user is blocked.
             TODO: refactor parsers._check_blocked() because if user is blocked, code-flow never gets there; was failing on the `#SessionID` selection in parsers.main_menu()
-            Called by login() """
+            Called by problem_check() """
         if 'you have been blocked' in resp_text.lower():
             self.blocked_patron = True
             self.registered = True
@@ -70,6 +84,35 @@ class IlliadSession( object ):
             return_val = False
         log.debug( 'return_val, `%s`' % return_val )
         return return_val
+
+    def check_blocked_2(self):
+        """ Hits a form url to check for blocked response.
+            Called by problem_check() """
+        dummy_openurl = 'Action=10&Form=30&sid=bul_illiad_api_blocked_test&genre=article'  # this shows the article form, one of the forms that would show a `blocked` status.
+        rslt_dct = self.get_request_key( dummy_openurl )
+        log.debug( 'rslt, ```%s```' % rslt_dct )
+        if rslt_dct.get( 'errors', None ):
+            if 'you have been blocked' in rslt_dct['errors'].lower():
+                self.blocked_patron = True
+                self.registered = True
+        return_val = self.blocked_patron
+        log.debug( 'return_val, `%s`' % return_val )
+        return return_val
+
+    # def check_blocked_2(self):
+    #     """ Hits a form url to check for blocked response.
+    #         Called by problem_check() """
+    #     dummy_openurl = 'Action=10&Form=30&sid=bul_illiad_api_blocked_test&genre=article'  # this shows the article form, one of the forms that would show a `blocked` status.
+    #     rslt_dct = self.get_request_key( dummy_openurl )
+    #     log.debug( 'rslt, ```%s```' % rslt_dct )
+    #     if 'you have been blocked' in rslt_dct.get( 'errors', '' ).lower():
+    #         self.blocked_patron = True
+    #         self.registered = True
+    #         return_val = True
+    #     else:
+    #         return_val = False
+    #     log.debug( 'return_val, `%s`' % return_val )
+    #     return return_val
 
     def _check_disavowed( self, resp_text ):
         """ Checks if login attempt indicates user is disavowed.
@@ -215,12 +258,72 @@ class IlliadSession( object ):
                           cookies=self.cookies,
                           verify=True,
                           timeout=15)
+        log.debug( 'resp.status_code, `%s`' % resp.status_code )
+        log.debug( 'new-user response, ```%s```' % resp.content )
         out = {}
-        #out['meta'] = r.content
+        if (resp.status_code is not 200) or ('statusError' in resp.content.decode('utf-8', 'replace')):  # `statusError` element or attribute (can't remember which) in html when, eg, a required element is missing (only seen for first-name and last-name)
+            self.registered = False
+            out['status'] = 'registration_failed'
+        else:
+            self.registered = True
+            out['status'] = 'Registered'
         out['status_code'] = resp.status_code
-        self.registered = True
-        out['status'] = 'Registered'
+        log.debug( 'register_user output for `%s`, ```%s```' % (self.username, pprint.pformat(out)) )
         return out
+
+        ## end def register_user()
+
+    # def register_user(self, user_dict, **kwargs):
+    #     """
+    #     user_dict contains the required information about the patron.
+
+    #     Pass in alternate keywords to specify other user attributes,
+    #     e.g: site="Medical Library"
+
+    #     """
+    #     #pull appropriate user dict variables.  Set some defaults.
+    #     first_name = user_dict.get('first_name', None)
+    #     last_name = user_dict.get('last_name', None)
+    #     email = user_dict.get('email', None)
+    #     #Faculty, staff, student, etct
+    #     status = user_dict.get('status', 'Student')
+    #     address = user_dict.get('address', 'See campus directory')
+    #     phone = user_dict.get('phone', 'N/A')
+    #     reg_key = {}
+    #     reg_key['SessionID'] = self.session_id
+    #     reg_key['ILLiadForm'] = 'ChangeUserInformation'
+    #     reg_key['Username'] = self.username
+    #     reg_key['FirstName'] = first_name
+    #     reg_key['LastName'] = last_name
+    #     reg_key['EMailAddress'] = email
+    #     reg_key['StatusGroup'] = status
+    #     reg_key['Phone'] = phone
+    #     reg_key['Address'] = address
+    #     #defaults
+    #     reg_key['NotifyGroup'] = 'E-Mail'
+    #     reg_key['DeliveryGroup'] = 'Electronic Delivery if Possible'
+    #     reg_key['LoanDeliveryGroup'] = 'Hold for Pickup'
+    #     reg_key['WebDeliveryGroup'] = 'Yes'
+    #     reg_key['Site'] = kwargs.get('site', 'Rockefeller Circ. Desk')
+    #     reg_key['NVTGC'] = 'ILL'
+    #     reg_key['SubmitButton'] = 'Submit Information'
+    #     reg_key['Department'] = kwargs.get('department', 'Other - Unlisted')
+
+    #     log.info("Registering %s with ILLiad as %s." % (self.username, status))
+
+    #     resp = requests.post(self.url,
+    #                       data=reg_key,
+    #                       headers=self.header,
+    #                       cookies=self.cookies,
+    #                       verify=True,
+    #                       timeout=15)
+    #     log.debug( 'new-user response, ```%s```' % resp.content )
+    #     out = {}
+    #     #out['meta'] = r.content
+    #     out['status_code'] = resp.status_code
+    #     self.registered = True
+    #     out['status'] = 'Registered'
+    #     return out
 
     ## end class class IlliadSession()
 
