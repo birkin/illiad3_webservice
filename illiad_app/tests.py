d20ff55b215fd7dfb6b1c0f933ca79c4aead83bb
birkin <birkin.diana@gmail.com>
2019-04-03 19:46:46 -0400

    stubs cloud-check-user client tests.

--- illiad_app/tests.py
+++ illiad_app/tests.py
@@ -173,6 +173,80 @@ class ClientCloudCreateUser_Test( TestCase ):
 #     ## end class ClientCheckUser_Test()
 
 
+class ClientCloudCheckUser_Test( TestCase ):
+    """ Tests views.check_user() -- for status meaning `blocked`, `registered`, etc. """
+
+    def test_check_good_existing_user(self):
+        """ Checks happy path. """
+        b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+        headers = {
+            'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+            'User-Agent': 'bul-test-client' }
+        c = Client()
+        response = c.get( '/cloud_check_user/', {'user': settings_app.TEST_EXISTING_GOOD_USER}, **headers )
+        self.assertEqual( 200, response.status_code )
+        jdct = json.loads( response.content )
+        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+        self.assertEqual(
+            {'authenticated': True, 'blocked': None, 'disavowed': None, 'registered': True, 'interpreted_new_user': False},
+            jdct['response']['status_data']
+            )
+
+    def test_check_disavowed_user(self):
+        """ Checks disavowed user. """
+        b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+        headers = {
+            'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+            'User-Agent': 'bul-test-client' }
+        c = Client()
+        response = c.get( '/cloud_check_user/', {'user': settings_app.TEST_DISAVOWED_USERNAME}, **headers )
+        self.assertEqual( 200, response.status_code )
+        jdct = json.loads( response.content )
+        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+        self.assertEqual(
+            {'authenticated': False, 'blocked': None, 'disavowed': True, 'registered': None, 'interpreted_new_user': False},
+            jdct['response']['status_data']
+            )
+
+    def test_check_blocked_user(self):
+        """ Checks blocked user. """
+        b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+        headers = {
+            'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+            'User-Agent': 'bul-test-client' }
+        c = Client()
+        response = c.get( '/cloud_check_user/', {'user': settings_app.TEST_BLOCKED_USERNAME}, **headers )
+        self.assertEqual( 200, response.status_code )
+        jdct = json.loads( response.content )
+        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+        self.assertEqual(
+            {'authenticated': True, 'blocked': True, 'disavowed': None, 'registered': True, 'interpreted_new_user': False},
+            jdct['response']['status_data']
+            )
+
+    def test_check_unregistered_user(self):
+        """ Checks unregistered user.
+            NOTE: any user 'new' to illiad is entered into the database and looks like this user.
+                  ...meaning that this test WILL CREATE A NEW USER """
+        b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+        headers = {
+            'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+            'User-Agent': 'bul-test-client' }
+        c = Client()
+        # response = c.get( '/cloud_check_user/', {'user': settings_app.TEST_UNREGISTERED_USERNAME}, **headers )
+        response = c.get( '/cloud_check_user/', {'user': '%s%s' % ( 'zzzz', random.randint(1111, 9999) )}, **headers )
+        self.assertEqual( 200, response.status_code )
+        jdct = json.loads( response.content )
+        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+        self.assertEqual(
+            {'authenticated': True, 'blocked': None, 'disavowed': None, 'registered': False, 'interpreted_new_user': True},
+            jdct['response']['status_data']
+            )
+
+    ## end class ClientCloudCheckUser_Test()
+
+
+
 class ClientV2_Test( TestCase ):
     """ Tests easyBorrow-api v2 """
 
