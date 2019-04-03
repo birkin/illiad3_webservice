00a089b256b39419a8db6497750df0cb3b96e4c2
birkin <birkin.diana@gmail.com>
2019-04-03 19:33:26 -0400

    old module tests disabled.

--- illiad_app/lib/illiad3/tests/parser_test.py
+++ illiad_app/lib/illiad3/tests/parser_test.py
@@ -1,139 +1,139 @@
-import os, sys, unittest
+# import os, sys, unittest
 
-## add project parent-directory to sys.path
-parent_working_dir = os.path.abspath( os.path.join(os.getcwd(), os.pardir) )
-sys.path.append( parent_working_dir )
+# ## add project parent-directory to sys.path
+# parent_working_dir = os.path.abspath( os.path.join(os.getcwd(), os.pardir) )
+# sys.path.append( parent_working_dir )
 
-from illiad_app.lib.illiad3 import parsers
-from illiad_app.lib.illiad3.parsers import UserInfoParser
-
-
-DATA_PATH = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), 'data'
-    )  # Directory where test-data is stored.
+# from illiad_app.lib.illiad3 import parsers
+# from illiad_app.lib.illiad3.parsers import UserInfoParser
 
 
-class ParserTest(unittest.TestCase):
-
-    def test_login(self):
-        path = os.path.join(DATA_PATH, 'login.html')
-        with open( path, 'rt' ) as f:
-            content = f.read()
-        login = parsers.main_menu(content)
-        self.assertTrue(login['authenticated'])
-        self.assertEqual(login['session_id'],
-                         'Q102112146D')
-
-    def test_needs_registration(self):
-        path = os.path.join(DATA_PATH, 'not_registered.html')
-        with open( path, 'rt' ) as f:
-            content = f.read()
-        login = parsers.main_menu(content)
-        self.assertFalse(login['registered'])
-
-    def test_article_request_key(self):
-        """
-        Test a few values for an article request key.
-        """
-        def _key_check(key_list, rdict):
-            for k in key_list:
-                self.assertTrue(k in rdict,
-                                msg="Request key missing key -- %s." % k)
-        path = os.path.join(DATA_PATH, 'article_request.html')
-        with open( path, 'rt' ) as f:
-            content = f.read()
-        self.assertEqual( str, type(content) )  # content is unicode
-        submit_details = parsers.request_form(content)
-        request_key = submit_details
-        _key_check(['PhotoJournalInclusivePages',
-                    'PhotoJournalYear',
-                    'PhotoJournalTitle'],
-                   request_key)
-        jtitle = request_key.get('PhotoJournalTitle')
-        self.assertEqual(jtitle,
-                         'Current pharmaceutical design')
-        atitle = request_key.get('PhotoArticleTitle')
-        self.assertTrue(u'Nicotinic Acetylcholine Receptors' in atitle)
-        issn = request_key.get('ISSN')
-        self.assertEqual(issn,
-                         u'1381-6128')
-
-    def test_blocked_patron(self):
-        path = os.path.join(DATA_PATH, 'blocked.html')
-        with open( path, 'rt' ) as f:
-            content = f.read()
-        request_key = parsers.request_form(content)
-        self.assertTrue(request_key['blocked'])
-        blocked_found = request_key['errors'].rfind('blocked')
-        self.assertNotEqual(blocked_found, -1)
-
-    def test_completed_request_submission(self):
-        path = os.path.join(DATA_PATH, 'completed_request.html')
-        with open( path, 'rt' ) as f:
-            content = f.read()
-        submit = parsers.request_submission(content)
-        self.assertFalse(submit['error'])
-        self.assertEqual(
-            submit['transaction_number'],
-            '476993'
-        )
-        self.assertTrue(submit['submitted'])
-
-    def test_failed_request_submission(self):
-        path = os.path.join(DATA_PATH, 'failed_request_missing_field.html')
-        with open( path, 'rt' ) as f:
-            content = f.read()
-        submit = parsers.request_submission(content)
-        self.assertTrue(submit['error'])
-        self.assertEqual(submit['message'],
-                         'Photo Journal Year is a required field.')
-
-    def test_parse_user_status(self):
-        """ Checks parsing. """
-        path = os.path.join( DATA_PATH, 'change_user_info.html' )
-        with open( path, 'rt' ) as f:
-            content = f.read()
-            self.assertEqual( str, type(content) )
-        self.assertEqual(
-            'Staff',
-            parsers.parse_user_status( content )
-            )
-
-    ## end class ParserTest()
+# DATA_PATH = os.path.join(
+#     os.path.dirname(os.path.realpath(__file__)), 'data'
+#     )  # Directory where test-data is stored.
 
 
-class UserInfoParserTest(unittest.TestCase):
+# class ParserTest(unittest.TestCase):
 
-    def setUp(self):
-        self.usr_prsr = UserInfoParser()
-        self.content = None
-        path = os.path.join( DATA_PATH, 'change_user_info.html' )
-        with open( path, 'rt' ) as f:
-            self.content = f.read()
+#     def test_login(self):
+#         path = os.path.join(DATA_PATH, 'login.html')
+#         with open( path, 'rt' ) as f:
+#             content = f.read()
+#         login = parsers.main_menu(content)
+#         self.assertTrue(login['authenticated'])
+#         self.assertEqual(login['session_id'],
+#                          'Q102112146D')
 
-    # def test_parse_first_name(self):
-    #     """ Checks first-name. """
-    #     self.assertEqual(
-    #         'the_first_name',
-    #         self.usr_prsr.parse_first_name( self.content )
-    #         )
+#     def test_needs_registration(self):
+#         path = os.path.join(DATA_PATH, 'not_registered.html')
+#         with open( path, 'rt' ) as f:
+#             content = f.read()
+#         login = parsers.main_menu(content)
+#         self.assertFalse(login['registered'])
 
-    def test_parse_input_element(self):
-        """ Checks last-name handling, as example. """
-        self.assertEqual(
-            'the_last_name',
-            self.usr_prsr.parse_input_element( submitted_html=self.content, target_id='LastName', target_attribute='value' )
-            )
+#     def test_article_request_key(self):
+#         """
+#         Test a few values for an article request key.
+#         """
+#         def _key_check(key_list, rdict):
+#             for k in key_list:
+#                 self.assertTrue(k in rdict,
+#                                 msg="Request key missing key -- %s." % k)
+#         path = os.path.join(DATA_PATH, 'article_request.html')
+#         with open( path, 'rt' ) as f:
+#             content = f.read()
+#         self.assertEqual( str, type(content) )  # content is unicode
+#         submit_details = parsers.request_form(content)
+#         request_key = submit_details
+#         _key_check(['PhotoJournalInclusivePages',
+#                     'PhotoJournalYear',
+#                     'PhotoJournalTitle'],
+#                    request_key)
+#         jtitle = request_key.get('PhotoJournalTitle')
+#         self.assertEqual(jtitle,
+#                          'Current pharmaceutical design')
+#         atitle = request_key.get('PhotoArticleTitle')
+#         self.assertTrue(u'Nicotinic Acetylcholine Receptors' in atitle)
+#         issn = request_key.get('ISSN')
+#         self.assertEqual(issn,
+#                          u'1381-6128')
+
+#     def test_blocked_patron(self):
+#         path = os.path.join(DATA_PATH, 'blocked.html')
+#         with open( path, 'rt' ) as f:
+#             content = f.read()
+#         request_key = parsers.request_form(content)
+#         self.assertTrue(request_key['blocked'])
+#         blocked_found = request_key['errors'].rfind('blocked')
+#         self.assertNotEqual(blocked_found, -1)
+
+#     def test_completed_request_submission(self):
+#         path = os.path.join(DATA_PATH, 'completed_request.html')
+#         with open( path, 'rt' ) as f:
+#             content = f.read()
+#         submit = parsers.request_submission(content)
+#         self.assertFalse(submit['error'])
+#         self.assertEqual(
+#             submit['transaction_number'],
+#             '476993'
+#         )
+#         self.assertTrue(submit['submitted'])
+
+#     def test_failed_request_submission(self):
+#         path = os.path.join(DATA_PATH, 'failed_request_missing_field.html')
+#         with open( path, 'rt' ) as f:
+#             content = f.read()
+#         submit = parsers.request_submission(content)
+#         self.assertTrue(submit['error'])
+#         self.assertEqual(submit['message'],
+#                          'Photo Journal Year is a required field.')
+
+#     def test_parse_user_status(self):
+#         """ Checks parsing. """
+#         path = os.path.join( DATA_PATH, 'change_user_info.html' )
+#         with open( path, 'rt' ) as f:
+#             content = f.read()
+#             self.assertEqual( str, type(content) )
+#         self.assertEqual(
+#             'Staff',
+#             parsers.parse_user_status( content )
+#             )
+
+#     ## end class ParserTest()
 
 
-    ## end class UserInfoParserTest()
+# class UserInfoParserTest(unittest.TestCase):
+
+#     def setUp(self):
+#         self.usr_prsr = UserInfoParser()
+#         self.content = None
+#         path = os.path.join( DATA_PATH, 'change_user_info.html' )
+#         with open( path, 'rt' ) as f:
+#             self.content = f.read()
+
+#     # def test_parse_first_name(self):
+#     #     """ Checks first-name. """
+#     #     self.assertEqual(
+#     #         'the_first_name',
+#     #         self.usr_prsr.parse_first_name( self.content )
+#     #         )
+
+#     def test_parse_input_element(self):
+#         """ Checks last-name handling, as example. """
+#         self.assertEqual(
+#             'the_last_name',
+#             self.usr_prsr.parse_input_element( submitted_html=self.content, target_id='LastName', target_attribute='value' )
+#             )
 
 
-def suite():
-    suite = unittest.makeSuite(ParserTest, 'test')
-    suite.addTest( unittest.makeSuite(UserInfoParserTest) )
-    return suite
+#     ## end class UserInfoParserTest()
 
 
-if __name__ == '__main__':
-    unittest.main()
+# def suite():
+#     suite = unittest.makeSuite(ParserTest, 'test')
+#     suite.addTest( unittest.makeSuite(UserInfoParserTest) )
+#     return suite
+
+
+# if __name__ == '__main__':
+#     unittest.main()
--- illiad_app/tests.py
+++ illiad_app/tests.py
@@ -56,121 +56,121 @@ class ClientCloudCreateUser_Test( TestCase ):
     ## end class ClientCloudCreateUser_Test()
 
 
-class ClientCreateUser_Test( TestCase ):
-    """ Tests views.create_user() """
+# class ClientCreateUser_Test( TestCase ):
+#     """ Tests views.create_user() """
 
-    def test_create_user__good_data(self):
-        """ Checks good data. """
-        c = Client()
-        params = {
-            'auth_key': settings_app.API_KEY,  # brown internal api
-            # 'auth_id': '%s%s' % ( 'zzzz', random.randint(1111, 9999) ),
-            'auth_id': settings_app.TEST_UNREGISTERED_USERNAME,  # functionally the line above is more accurate, but this is fine for regular automated tests.
-            'first_name': 'the-first-name',
-            'last_name': 'the-last-name',
-            'email': 'bar'
-            }
-        response = c.post( '/create_user/', params )
-        self.assertEqual( 200, response.status_code )
-        jdct = json.loads( response.content )
-        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
-        self.assertEqual( ['params', 'timestamp', 'url'], sorted(list(jdct['request'].keys())) )
-        self.assertEqual( ['elapsed_time', 'status_data'], sorted(list(jdct['response'].keys())) )
-        self.assertEqual( {'status': 'Registered', 'status_code': 200}, jdct['response']['status_data'] )
+#     def test_create_user__good_data(self):
+#         """ Checks good data. """
+#         c = Client()
+#         params = {
+#             'auth_key': settings_app.API_KEY,  # brown internal api
+#             # 'auth_id': '%s%s' % ( 'zzzz', random.randint(1111, 9999) ),
+#             'auth_id': settings_app.TEST_UNREGISTERED_USERNAME,  # functionally the line above is more accurate, but this is fine for regular automated tests.
+#             'first_name': 'the-first-name',
+#             'last_name': 'the-last-name',
+#             'email': 'bar'
+#             }
+#         response = c.post( '/create_user/', params )
+#         self.assertEqual( 200, response.status_code )
+#         jdct = json.loads( response.content )
+#         self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+#         self.assertEqual( ['params', 'timestamp', 'url'], sorted(list(jdct['request'].keys())) )
+#         self.assertEqual( ['elapsed_time', 'status_data'], sorted(list(jdct['response'].keys())) )
+#         self.assertEqual( {'status': 'Registered', 'status_code': 200}, jdct['response']['status_data'] )
 
-    def test_create_user__bad_data(self):
-        """ Checks bad data. """
-        c = Client()
-        response = c.post( '/create_user/', {'aa': 'foo_a', 'bb': 'foo_b'} )
-        self.assertEqual( 400, response.status_code )
+#     def test_create_user__bad_data(self):
+#         """ Checks bad data. """
+#         c = Client()
+#         response = c.post( '/create_user/', {'aa': 'foo_a', 'bb': 'foo_b'} )
+#         self.assertEqual( 400, response.status_code )
 
-    def test_create_user__missing_data(self):
-        """ Checks missing data. """
-        c = Client()
-        params = {
-            'auth_key': settings_app.API_KEY,  # brown internal api
-            'auth_id': settings_app.TEST_UNREGISTERED_USERNAME,
-            # 'first_name': 'the-first-name',  # first_name is required
-            'last_name': 'the-last-name',
-            'email': 'bar'
-            }
-        response = c.post( '/create_user/', params )
-        self.assertEqual( 400, response.status_code )
+#     def test_create_user__missing_data(self):
+#         """ Checks missing data. """
+#         c = Client()
+#         params = {
+#             'auth_key': settings_app.API_KEY,  # brown internal api
+#             'auth_id': settings_app.TEST_UNREGISTERED_USERNAME,
+#             # 'first_name': 'the-first-name',  # first_name is required
+#             'last_name': 'the-last-name',
+#             'email': 'bar'
+#             }
+#         response = c.post( '/create_user/', params )
+#         self.assertEqual( 400, response.status_code )
 
-    ## end class ClientCreateUser_Test()
+#     ## end class ClientCreateUser_Test()
 
 
-class ClientCheckUser_Test( TestCase ):
-    """ Tests views.check_user() -- for status meaning `blocked`, `registered`, etc. """
+# class ClientCheckUser_Test( TestCase ):
+#     """ Tests views.check_user() -- for status meaning `blocked`, `registered`, etc. """
 
-    def test_check_good_existing_user(self):
-        """ Checks happy path. """
-        b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
-        headers = {
-            'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
-            'User-Agent': 'bul-test-client' }
-        c = Client()
-        response = c.get( '/check_user/', {'user': settings_app.TEST_EXISTING_GOOD_USER}, **headers )
-        self.assertEqual( 200, response.status_code )
-        jdct = json.loads( response.content )
-        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
-        self.assertEqual(
-            {'authenticated': True, 'blocked': None, 'disavowed': None, 'registered': True, 'interpreted_new_user': False},
-            jdct['response']['status_data']
-            )
+#     def test_check_good_existing_user(self):
+#         """ Checks happy path. """
+#         b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+#         headers = {
+#             'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+#             'User-Agent': 'bul-test-client' }
+#         c = Client()
+#         response = c.get( '/check_user/', {'user': settings_app.TEST_EXISTING_GOOD_USER}, **headers )
+#         self.assertEqual( 200, response.status_code )
+#         jdct = json.loads( response.content )
+#         self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+#         self.assertEqual(
+#             {'authenticated': True, 'blocked': None, 'disavowed': None, 'registered': True, 'interpreted_new_user': False},
+#             jdct['response']['status_data']
+#             )
 
-    def test_check_disavowed_user(self):
-        """ Checks disavowed user. """
-        b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
-        headers = {
-            'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
-            'User-Agent': 'bul-test-client' }
-        c = Client()
-        response = c.get( '/check_user/', {'user': settings_app.TEST_DISAVOWED_USERNAME}, **headers )
-        self.assertEqual( 200, response.status_code )
-        jdct = json.loads( response.content )
-        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
-        self.assertEqual(
-            {'authenticated': False, 'blocked': None, 'disavowed': True, 'registered': None, 'interpreted_new_user': False},
-            jdct['response']['status_data']
-            )
+#     def test_check_disavowed_user(self):
+#         """ Checks disavowed user. """
+#         b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+#         headers = {
+#             'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+#             'User-Agent': 'bul-test-client' }
+#         c = Client()
+#         response = c.get( '/check_user/', {'user': settings_app.TEST_DISAVOWED_USERNAME}, **headers )
+#         self.assertEqual( 200, response.status_code )
+#         jdct = json.loads( response.content )
+#         self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+#         self.assertEqual(
+#             {'authenticated': False, 'blocked': None, 'disavowed': True, 'registered': None, 'interpreted_new_user': False},
+#             jdct['response']['status_data']
+#             )
 
-    def test_check_blocked_user(self):
-        """ Checks blocked user. """
-        b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
-        headers = {
-            'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
-            'User-Agent': 'bul-test-client' }
-        c = Client()
-        response = c.get( '/check_user/', {'user': settings_app.TEST_BLOCKED_USERNAME}, **headers )
-        self.assertEqual( 200, response.status_code )
-        jdct = json.loads( response.content )
-        self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
-        self.assertEqual(
-            {'authenticated': True, 'blocked': True, 'disavowed': None, 'registered': True, 'interpreted_new_user': False},
-            jdct['response']['status_data']
-            )
+#     def test_check_blocked_user(self):
+#         """ Checks blocked user. """
+#         b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+#         headers = {
+#             'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+#             'User-Agent': 'bul-test-client' }
+#         c = Client()
+#         response = c.get( '/check_user/', {'user': settings_app.TEST_BLOCKED_USERNAME}, **headers )
+#         self.assertEqual( 200, response.status_code )
+#         jdct = json.loads( response.content )
+#         self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+#         self.assertEqual(
+#             {'authenticated': True, 'blocked': True, 'disavowed': None, 'registered': True, 'interpreted_new_user': False},
+#             jdct['response']['status_data']
+#             )
 
-    # def test_check_unregistered_user(self):
-    #     """ Checks unregistered user.
-    #         NOTE: any user 'new' to illiad is entered into the database and looks like this user.
-    #               ...meaning that this test WILL CREATE A NEW USER """
-    #     b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
-    #     headers = {
-    #         'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
-    #         'User-Agent': 'bul-test-client' }
-    #     c = Client()
-    #     # response = c.get( '/check_user/', {'user': settings_app.TEST_UNREGISTERED_USERNAME}, **headers )
-    #     response = c.get( '/check_user/', {'user': '%s%s' % ( 'zzzz', random.randint(1111, 9999) )}, **headers )
-    #     self.assertEqual( 200, response.status_code )
-    #     jdct = json.loads( response.content )
-    #     self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
-    #     self.assertEqual(
-    #         {'authenticated': True, 'blocked': None, 'disavowed': None, 'registered': False, 'interpreted_new_user': True},
-    #         jdct['response']['status_data']
-    #         )
+#     # def test_check_unregistered_user(self):
+#     #     """ Checks unregistered user.
+#     #         NOTE: any user 'new' to illiad is entered into the database and looks like this user.
+#     #               ...meaning that this test WILL CREATE A NEW USER """
+#     #     b64_bytes = base64.b64encode( b'%s:%s' % (settings_app.BASIC_AUTH_USER.encode('utf-8'), settings_app.BASIC_AUTH_PASSWORD.encode('utf-8')) )
+#     #     headers = {
+#     #         'HTTP_AUTHORIZATION': 'Basic ' + b64_bytes.decode('utf-8'),
+#     #         'User-Agent': 'bul-test-client' }
+#     #     c = Client()
+#     #     # response = c.get( '/check_user/', {'user': settings_app.TEST_UNREGISTERED_USERNAME}, **headers )
+#     #     response = c.get( '/check_user/', {'user': '%s%s' % ( 'zzzz', random.randint(1111, 9999) )}, **headers )
+#     #     self.assertEqual( 200, response.status_code )
+#     #     jdct = json.loads( response.content )
+#     #     self.assertEqual( ['request', 'response'], sorted(list(jdct.keys())) )
+#     #     self.assertEqual(
+#     #         {'authenticated': True, 'blocked': None, 'disavowed': None, 'registered': False, 'interpreted_new_user': True},
+#     #         jdct['response']['status_data']
+#     #         )
 
-    ## end class ClientCheckUser_Test()
+#     ## end class ClientCheckUser_Test()
 
 
 class ClientV2_Test( TestCase ):
